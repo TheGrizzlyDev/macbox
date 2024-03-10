43300897c08929291f0b9ddf7ff6f981b2621e80
TheGrizzlyDev <tonio9681@gmail.com>
2024-03-10 19:31:34 +0000

    Add serialization and deserialization to protobuf

--- libsystem/protobuf/bindings.zig
+++ libsystem/protobuf/bindings.zig
@@ -8,7 +8,22 @@ const c = @cImport({
 const ProtobufErrors = error{
     CannotAllocate,
     CannotParse,
+    CannotSerialize,
 };
+
+fn SliceWithUpbArena(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        slice: []const T,
+        arena: *arena.AllocatorBackedUpbArena,
+        alloc: *const std.mem.Allocator,
+
+        pub fn deinit(self: *Self) void {
+            self.arena.deinit();
+            self.alloc.destroy(self);
+        }
+    };
+}
 
 fn bindToProto(comptime fullyQualifiedName: []const u8) type {
     const upbNamespace: []const u8 = blk: {
@@ -46,18 +61,45 @@ fn bindToProto(comptime fullyQualifiedName: []const u8) type {
             return ProtobufErrors.CannotAllocate;
         }
 
-        // pub fn deserialize(allocator: *const std.mem.Allocator, ser: []const u8) ProtobufErrors!Self {
-        //     var newArena = try arena.AllocatorBackedUpbArena.init(allocator);
-        //     errdefer newArena.deinit();
-        //     if (c.macbox_core_v1_ApiRequest_parse(ser.ptr, ser.len, arena)) |msg| {
-        //         return .{ .msg = msg, .arena = newArena };
-        //     }
-        //     return ProtobufErrors.CannotParse;
-        // }
+        pub fn deserialize(allocator: *const std.mem.Allocator, buf: []const u8) !*Self {
+            var self = try allocator.create(Self);
+            errdefer allocator.destroy(self);
+            self.alloc = allocator;
+
+            var newArena = try arena.AllocatorBackedUpbArena.init(allocator);
+            errdefer newArena.deinit();
+
+            self.arena = newArena;
+            const upbArena: *c.upb_Arena = @ptrCast(newArena.upbArena());
+            if (@call(.auto, @field(c, std.fmt.comptimePrint("{s}_parse", .{upbNamespace})), .{ buf.ptr, buf.len, upbArena })) |msg| {
+                self.msg = msg;
+                return self;
+            }
+            return ProtobufErrors.CannotParse;
+        }
 
         pub fn deinit(self: *Self) void {
             self.arena.deinit();
             self.alloc.destroy(self);
+        }
+
+        pub fn serialize(self: *Self, allocator: *const std.mem.Allocator) !*SliceWithUpbArena(u8) {
+            var slice = try allocator.create(SliceWithUpbArena(u8));
+            errdefer allocator.destroy(slice);
+            slice.alloc = allocator;
+
+            var newArena = try arena.AllocatorBackedUpbArena.init(allocator);
+            errdefer newArena.deinit();
+            slice.arena = newArena;
+
+            var size: usize = 0;
+            const upbArena: *c.upb_Arena = @ptrCast(newArena.upbArena());
+            if (@call(.auto, @field(c, std.fmt.comptimePrint("{s}_serialize", .{upbNamespace})), .{ self.msg, upbArena, &size })) |msg| {
+                slice.slice = msg[0..size];
+                return slice;
+            }
+
+            return ProtobufErrors.CannotSerialize;
         }
     };
 }
@@ -66,6 +108,10 @@ test "bindToProto" {
     const DynamicApiRequest = bindToProto("macbox.core.v1.ApiRequest");
     var req = try DynamicApiRequest.init(&std.testing.allocator);
     defer req.deinit();
+    var serialized = try req.serialize(&std.testing.allocator);
+    defer serialized.deinit();
+    var deserialized = try DynamicApiRequest.deserialize(&std.testing.allocator, serialized.slice);
+    defer deserialized.deinit();
 }
 
 // pub const ApiRequest = struct {
